921de330648e41f281a87c30651ef0bc23239409
weng lin <wenglin@buffalo.edu>
2019-12-23 11:38:22 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,20 +2,19 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-
   ret_hash= {}
-   index = 0
-   while nds[index] do
-     sum = 0
-     num_movies=0
-     while nds[index][:movies][num_movies] do
-       sum += nds[index][:movies][num_movies][:worldwide_gross]
-       num_movies += 1
-     end
-     ret_hash[nds[index][:name]]= sum
-     index += 1
-   end
+  index = 0
+  while nds[index] do
+    sum = 0
+    num_movies=0
+    while nds[index][:movies][num_movies] do
+      sum += nds[index][:movies][num_movies][:worldwide_gross]
+      num_movies += 1
+    end
+    ret_hash[nds[index][:name]]= sum
+    index += 1
+  end
 
-   nil
-   return ret_hash
+  nil
+  return ret_hash
  end
