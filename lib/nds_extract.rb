8ff1df943a9ff873c5b94f1d7702774886c5470b
weng lin <wenglin@buffalo.edu>
2020-01-01 09:53:06 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,7 @@ def directors_totals(nds)
   index = 0
   while nds[index] do
     sum = 0
-    num_movies=0
+    num_movies = 0
     while nds[index][:movies][num_movies] do
       sum += nds[index][:movies][num_movies][:worldwide_gross]
       num_movies += 1
